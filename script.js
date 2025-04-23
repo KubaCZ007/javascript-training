cf6b82b4e7bd1073a9888f00f1fea330f5a221d7
Kuba <rybaj@ssemi.cz>
2025-04-23 13:11:45 +0200

    do videa 10

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
-console.log("vybuch");
-console.log("bum");
+/*
+console.log("vybuch")
+console.log("bum")
+
+let firstName = "Kuba"
+let secondName = "Ryba"
+let city = "Hradešín"
+console.log(firstName + " " + secondName + ", " + city)
+console.log(firstName)
+
+cvičení 6
+let firstName = "kuba"
+let job = "programátor"
+console.log("ahoj, jmenuji se " + firstName +" a pracuji jako " + job)
+*/
+
+// string
+// let firstName = "kuba"
+
+// //number
+// let age = 18
+// console.log(age)
+
+// početní operace
+// let x = (age + 5) * 2
+// console.log(x)
+
+/*
+cvičení 8
+let firstName = "kuba"
+let age = 18
+console.log("Jmenuji se "+ firstName + " a je mi " + age );
+let x = age + 5
+console.log("Za pět let mi bude "+ x);
+*/
+
+//cvičení 10
+
+let student1 = 48
+let student2 = 67
+let student1Name = "tonda"
+let student2Name = "pepa"
+let maxPoints = 100
+result1 = (student1 / maxPoints) * 100
+result2 = (student2 / maxPoints) * 100
+
+console.log(student1Name + " dosáhl(a) " + result1 + "% úspěšnosti");
+console.log(student2Name + " dosáhl(a) " + result2 + "% úspěšnosti");
+
+
